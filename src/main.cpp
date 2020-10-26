da3d067870922b7f51115369d7e3bd55b77009d9
Luke Pearson <luke.pearson@mongodb.com>
2020-10-26 16:13:28 +1100

    Updating to use more basic json library and loading in single document

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@
  */
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include "json.hpp"
 #include "wiredtiger.h"
 
 // Some files included by wt_internal.h have some C-ism's that don't work in C++.
@@ -20,11 +24,21 @@ extern "C" {
 #include "misc.h"
 }
 
+using namespace nlohmann;
+
+int load_file() {
+    std::ifstream ifs("../raw_data/rockbench_1row.json");
+    json jf = json::parse(ifs);
+    std::cout << jf.at("data");
+    return 0;
+}
+
 int main()
 {
+    load_file();
+
     WT_CONNECTION *conn = nullptr;
     std::string dbpath = "wt_test";
-
     int ret = wiredtiger_open(dbpath.c_str(), nullptr, "create", &conn);
     if (ret != 0)
         std::cout << "wiredtiger_open failed with return code " << ret << '\n';
