3fd889dd53828bf3b5b70527734baedd6dc12a2a
Luke Pearson <luke.pearson@mongodb.com>
2020-10-27 14:20:03 +1100

    Adding insertion functions and calling them from json parsing function

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <unordered_map>
 #include <fstream>
 #include "json.hpp"
 #include "wiredtiger.h"
@@ -26,20 +27,60 @@ extern "C" {
 
 using namespace nlohmann;
 
-void process_json(json jsn) {
+enum Type { BOOL, NUMBER, STRING, OBJECT, ARRAY, EMPTY };
+
+
+Type convert_to_type(std::string type_name) {
+    if (type_name == "string")
+        return STRING;
+    if (type_name == "object")
+        return OBJECT;
+    if (type_name == "number")
+        return NUMBER;
+    if (type_name == "array")
+        return ARRAY;
+    if (type_name == "boolean")
+        return BOOL;
+    return STRING;
+}
+
+void insert_column(std::string key, uint64_t id, json value) {
+    Type t = convert_to_type(value.type_name());
+    std::cout << "Inserting: " << key <<" " << id << " " << value << " " << t << std::endl;
+}
+
+void insert_row(uint64_t id, std::string key, json value) {
+    Type t = convert_to_type(value.type_name());
+    std::cout << "Inserting: " << id << " " << key <<" " << value << " " << t << std::endl;
+}
+
+std::unordered_map<std::string, json> process_sub_json(json jsn) {
+    std::unordered_map<std::string, json> kvs{};
+
+    return kvs;
+}
+
+void process_json(json jsn, uint64_t id) {
     std::cout << std::endl;
     for (auto it : jsn.at("data").at(0).items()) {
-        std::cout << it.value().type_name() << " " << it.key()  << std::endl;
+        //std::cout << it.value().type_name() << " " << it.key()  << std::endl;
         if (it.value().is_boolean()) {
-
+            insert_row(id, it.key(), it.value());
+            insert_column(it.key(), id, it.value());
         } else if (it.value().is_number()) {
-
+            insert_row(id, it.key(), it.value());
+            insert_column(it.key(), id, it.value());
         } else if (it.value().is_object()) {
-
+            for (auto itt : process_sub_json(it.value())) {
+                insert_row(id, itt.first, itt.second);
+                insert_column(itt.first, id, itt.second);
+            }
         } else if (it.value().is_string()) {
-
+            insert_row(id, it.key(), it.value());
+            insert_column(it.key(), id, it.value());
         } else if (it.value().is_array()) {
-
+            insert_row(id, it.key(), it.value());
+            insert_column(it.key(), id, it.value());
         } else if (it.value().is_null()) {
 
         }
@@ -47,10 +88,12 @@ void process_json(json jsn) {
 }
 
 void load_file() {
+    uint64_t id = 0;
     std::string line;
     std::ifstream ifs("../raw_data/rockbench_10rows.json");
     while (std::getline(ifs, line)) {
-        process_json(json::parse(line));
+        process_json(json::parse(line), id);
+        id++;
     }
 }
 
