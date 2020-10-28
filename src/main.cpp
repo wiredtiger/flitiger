ecea702bac6e5a7ff62ddb16ef18be6bb26cfa2d
Luke Pearson <luke.pearson@mongodb.com>
2020-10-28 11:47:56 +1100

    Switch json libraries

--- src/main.cpp
+++ src/main.cpp
@@ -10,104 +10,77 @@
 #include <string>
 #include <fstream>
 #include <cassert>
-#include "json.hpp"
+#include <cpprest/json.h>
 
 #include "wiredtiger.h"
 #include "RestIngestServer.hpp"
 
 #include "wt.h"
 
-using namespace nlohmann;
-
-enum Type { BOOL, NUMBER, STRING, OBJECT, ARRAY, EMPTY };
 std::string rtbl = "table:row_table";
 std::string ctbl = "table:col_table";
 
-Type convert_to_type(std::string type_name) {
-    if (type_name == "string")
-        return STRING;
-    if (type_name == "object")
-        return OBJECT;
-    if (type_name == "number")
-        return NUMBER;
-    if (type_name == "array")
-        return ARRAY;
-    if (type_name == "boolean")
-        return BOOL;
-    return STRING;
-}
-
-Type get_type(json value) {
-
-    std::string type_name = value.type_name();
-    if (type_name == "object")
-        return OBJECT;
-    if (type_name == "number")
-        return NUMBER;
-    if (type_name == "array")
-        return ARRAY;
-    if (type_name == "boolean")
-        return BOOL;
-    return STRING;
-}
-
-int insert_column(WT_CURSOR *cursor, const std::string &key, uint64_t id, json value) {
-    
-    int ret = 0;    
-    WT_ITEM item;
-    auto val = value.dump();
-    item.data = val.data();
-    item.size = val.length();
-    if ((ret = wt::col_table_insert(cursor, key, id, get_type(value), item)) == 0) {
-#if 0
-        std::cout << "insert_column: (" << key << ", " << id << "), "
-                  << "(" << value.type_name() << ", " << val.data() << ")\n";
-#endif
-    }
-    return ret;
-}
-
-int insert_row(WT_CURSOR *cursor, uint64_t id, const std::string &key, json value) {
-
+int insert_column(WT_CURSOR *cursor, const std::string &key, uint64_t id, web::json::value value) {
     int ret = 0;
     WT_ITEM item;
-    auto val = value.dump();
+    auto val = value.serialize();
     item.data = val.data();
     item.size = val.length();
-    if ((ret = wt::row_table_insert(cursor, id, key, get_type(value), item)) == 0) {
+    if ((ret = wt::col_table_insert(cursor, key, id, value.type(), item)) == 0) {
 #if 0
-        //std::cout << "insert_row: (" << id << ", " << key << "), "
-          //        << "(" << value.type_name() << ", " << val.data() << ")\n";
+        std::cout << "insert_column: (" << key << ", " << id << "), "
+                  << "(" << value.type() << ", " << val.data() << ")\n";
 #endif
     }
     return ret;
 }
 
-std::unordered_map<std::string, json> process_sub_json(json jsn) {
-    std::unordered_map<std::string, json> kvs{};
+int insert_row(WT_CURSOR *cursor, uint64_t id, const std::string &key, web::json::value value) {
+    int ret = 0;
+    WT_ITEM item;
+    auto val = value.serialize();
+    item.data = val.data();
+    item.size = val.length();
+    if ((ret = wt::row_table_insert(cursor, id, key, value.type(), item)) == 0) {
+#if 0
+        //std::cout << "insert_row: (" << id << ", " << key << "), "
+          //        << "(" << value.type() << ", " << val.data() << ")\n";
+#endif
+    }
+    return ret;
+}
+
+std::unordered_map<std::string, web::json::value> process_sub_json(web::json::object jsn) {
+    std::unordered_map<std::string, web::json::value> kvs{};
 
     return kvs;
 }
 
-void process_json(WT_SESSION *session, json jsn, uint64_t id) {
+void process_json(WT_SESSION *session, web::json::value jsn, uint64_t *id) {
 
     WT_CURSOR *rc = nullptr;
     WT_CURSOR *cc = nullptr;
     wt::open_cursor(session, rtbl, &rc);
     wt::open_cursor(session, ctbl, &cc);
 
-    for (auto it : jsn.at("data").at(0).items()) {
-        if (it.value().is_null()) {
-            continue;
-        } else if (it.value().is_object()) {
-            for (auto itt : process_sub_json(it.value())) {
-                insert_row(rc, id, itt.first, itt.second);
-                insert_column(cc, itt.first, id, itt.second);
+    // The top level data is an array so iterate over it.
+    for (auto it : jsn.at("data").as_array()) {
+        for (auto itt : it.as_object()) {
+            auto val = itt.second;
+            auto key = itt.first;
+            if (val.is_null()) {
+                continue;
+            } else if (val.is_object()) {
+               /* for (auto itt : process_sub_json(val)) {
+                    insert_row(rc, id, itt.first, itt.second);
+                    insert_column(cc, itt.first, id, itt.second);
+                }*/
+            } else {
+                insert_row(rc, *id, key, val);
+                insert_column(cc, key, *id, val);
             }
-        } else {
-            insert_row(rc, id, it.key(), it.value());
-            insert_column(cc, it.key(), id, it.value());
         }
+        *id++;
     }
     wt::close_cursor(rc);
     wt::close_cursor(cc);
@@ -119,7 +92,7 @@ void load_file(WT_SESSION *session, const char *filename, uint64_t db_document_i
     std::string line;
     std::ifstream ifs(filename);
     while (std::getline(ifs, line)) {
-        process_json(session, json::parse(line), id);
+        process_json(session, web::json::value::parse(line), &id);
         id++;
     }
 }
